0c187bd51c8d2e449fa1c349a96d4135b78dcac7
jiangy <jiangliu0316@outlook.com>
2025-01-06 14:56:29 +0800

    feat: 完善文章对代码的高亮提示

--- features/quickstart/src/main/resources/rawfile/h5/js/index.js
+++ features/quickstart/src/main/resources/rawfile/h5/js/index.js
@@ -5,4 +5,11 @@ function openDialog(msg) {
 function loadData(body) {
   let el = document.getElementById("content");
   el.innerHTML = body;
+
+  if (hljs) {
+    hljs.highlightAll();
+    console.log("highlightAll 调用完成")
+  } else {
+    alert("hljs 加载未完成")
+  }
 }
